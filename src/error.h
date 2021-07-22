b10be621c443d4c8cb802298cf216c2cac245c75
folkert van heusden <mail@vanheusden.com>
2021-07-22 10:28:18 +0200

    error_exit does not return

--- src/error.h
+++ src/error.h
@@ -1,4 +1,4 @@
 // (C) 2017-2020 by folkert van heusden, released under AGPL v3.0
 #pragma once
-void error_exit(const bool se, const char *format, ...);
+void error_exit(const bool se, const char *format, ...) __attribute__ ((__noreturn__));
 void cpt(const int err);
