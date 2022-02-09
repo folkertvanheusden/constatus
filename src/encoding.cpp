e5ae2e71587da27d929ad4d80585115cd5b2248f
folkert van heusden <mail@vanheusden.com>
2022-02-09 10:34:56 +0100

    made yuy2_to_rgb 0.9% faster

--- src/encoding.cpp
+++ src/encoding.cpp
@@ -12,28 +12,26 @@ void yuy2_to_rgb(const uint8_t *const in, const int width, const int height, uin
 
 	uint8_t *out_work = *out = (uint8_t *)malloc(n_bytes_out);
 
-	for(int y=0; y<height; y++) {
-		for(int i = 0; i < width/2; i++) {
-			int y0 = in_work[0];
-			int u0 = in_work[1];
-			int y1 = in_work[2];
-			int v0 = in_work[3];
-			in_work += 4;
+	const int n = height * width / 2;
+	for(int loop=0; loop<n; loop++) {
+		int y0 = *in_work++;
+		int u0 = *in_work++;
+		int y1 = *in_work++;
+		int v0 = *in_work++;
 
-			int c = y0 - 16;
-			int d = u0 - 128;
-			int e = v0 - 128;
-			out_work[0] = std::clamp((298 * c + 409 * e + 128) >> 8, 0, 255); // red
-			out_work[1] = std::clamp((298 * c - 100 * d - 208 * e + 128) >> 8, 0, 255); // green
-			out_work[2] = std::clamp((298 * c + 516 * d + 128) >> 8, 0, 255); // blue
+		int c = y0 - 16;
+		int d = u0 - 128;
+		int e = v0 - 128;
+		int c298 = c * 298;
+		*out_work++ = std::clamp((c298 + 409 * e + 128) >> 8, 0, 255); // red
+		*out_work++ = std::clamp((c298 - 100 * d - 208 * e + 128) >> 8, 0, 255); // green
+		*out_work++ = std::clamp((c298 + 516 * d + 128) >> 8, 0, 255); // blue
 
-			c = y1 - 16;
-			out_work[3] = std::clamp((298 * c + 409 * e + 128) >> 8, 0, 255); // red
-			out_work[4] = std::clamp((298 * c - 100 * d - 208 * e + 128) >> 8, 0, 255); // green
-			out_work[5] = std::clamp((298 * c + 516 * d + 128) >> 8, 0, 255); // blue
-
-			out_work += 6;
-		}
+		c = y1 - 16;
+		c298 = c * 298;
+		*out_work++ = std::clamp((c298 + 409 * e + 128) >> 8, 0, 255); // red
+		*out_work++ = std::clamp((c298 - 100 * d - 208 * e + 128) >> 8, 0, 255); // green
+		*out_work++ = std::clamp((c298 + 516 * d + 128) >> 8, 0, 255); // blue
 	}
 }
 
