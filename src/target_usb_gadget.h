2d1e04b350310bdbf8c4cae7214e28048726589b
folkert van heusden <folkert@vanheusden.com>
2023-12-07 14:33:54 +0100

    wip

--- src/target_usb_gadget.h
+++ src/target_usb_gadget.h
@@ -2,18 +2,8 @@
 #pragma once
 
 #include "config.h"
-
-#if HAVE_USBGADGET == 1
 #include <optional>
 #include <string>
-
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-
-#include <usbg/usbg.h>
-#include <usbg/function/uvc.h>
 
 #include "target.h"
 
@@ -28,18 +18,11 @@ private:
 
 	const int quality      { 0 };
 
-        usbg_state  *ug_state  { nullptr };
-        usbg_gadget *g         { nullptr };
-        usbg_config *c         { nullptr };
-        usbg_function *f_uvc   { nullptr };
-
-	std::optional<std::string> setup();
-	void                       process_event(const int fd);
+	std::string uvc_id;
 
 public:
-	target_usbgadget(const std::string & id, const std::string & descr, source *const s, const int width, const int height, const double interval, const std::vector<filter *> *const filters, const double override_fps, configuration_t *const cfg, const int quality, const bool handle_failure, schedule *const sched);
+	target_usbgadget(const std::string & id, const std::string & descr, source *const s, const int width, const int height, const double interval, const std::vector<filter *> *const filters, const double override_fps, configuration_t *const cfg, const int quality, const bool handle_failure, schedule *const sched, const std::string & uvc_id);
 	virtual ~target_usbgadget();
 
 	void operator()() override;
 };
-#endif
